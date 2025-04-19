3f7adffac6ae97e703a76575987486339babb07f
renanjatczak <151166786+renanjatczak@users.noreply.github.com>
2025-04-19 15:50:17 -0300

    002

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/action', function() {
     return ('Olá mundo!');
 });
+
+Route::get('/controller', [MainController::class, 'index']);
