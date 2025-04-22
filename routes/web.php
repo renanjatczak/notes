c360babe14f6c12d2b48d69b5f365118c8e05853
renanjatczak <151166786+renanjatczak@users.noreply.github.com>
2025-04-22 10:33:14 -0300

    006

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,3 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('Main');
-});
-
-Route::get('/action', function() {
-    return ('Olá mundo!');
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
-
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-
-Route::get('/page3/{value}', [MainController::class, 'page3']);
-
