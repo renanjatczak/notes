fbe8a6ed9f534513b2375d0c19195e88d26cf3fb
renanjatczak <151166786+renanjatczak@users.noreply.github.com>
2025-04-25 10:41:12 -0300

    018

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
 // auth routes (rotas de autenticação)
+
 //Serão executadas se o usuário não existir ou não estiver logado.
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::middleware([CheckIsNotLogged::class])->group(function(){
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
 
 //Só serão executadas se o usuário estiver logado
 Route::middleware([CheckIsLogged::class])->group(function(){
