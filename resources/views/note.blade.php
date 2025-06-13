4fa34dc69d41a79613cb461b9b936291d851b3ac
renanjatczak <151166786+renanjatczak@users.noreply.github.com>
2025-06-13 13:55:37 -0300

    Edit e Delete funcionando + modal de confirmação no delete e logout

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -4,17 +4,54 @@
             <div class="row">
                 <div class="col">
                     <h4 class="text-info">{{ $note['title'] }}</h4>
-                    <small class="text-secondary"><span class="opacity-75 me-2">Created at:</span><strong>{{ date('d/m/Y - H:i', strtotime( $note['created_at'])) }}</strong></small>
+                    <small class="text-secondary">
+                        <span class="opacity-75 me-2">Created at:</span>
+                        <strong>{{ date('Y-m-d H:i:s', strtotime($note['created_at'])) }}</strong>
+                    </small>
+
+                    @if($note['created_at'] != $note['updated_at'])
+                        <small class="text-secondary ms-5">
+                            <span class="opacity-75 me-2">Updated at:</span>
+                            <strong>{{ date('Y-m-d H:i:s', strtotime($note['updated_at'])) }}</strong>
+                        </small>
+                    @endif
                 </div>
+
                 <div class="col text-end">
-                    <a href="{{ route('edit', ['id' => Crypt::encrypt($note['id'])]) }}" class="btn btn-outline-secondary btn-sm mx-1"><i class="fa-regular fa-pen-to-square"></i></a>
-                    <a href="{{ route('delete', ['id' => Crypt::encrypt($note['id'])]) }}" class="btn btn-outline-danger btn-sm mx-1"><i class="fa-regular fa-trash-can"></i></a>
+                    <!-- Botão de edição -->
+                    <a href="{{ route('edit', ['id' => Crypt::encrypt($note['id'])]) }}" class="btn btn-outline-secondary btn-sm mx-1">
+                        <i class="fa-regular fa-pen-to-square"></i>
+                    </a>
+
+                    <!-- Botão que abre o modal de exclusão -->
+                    <button type="button" class="btn btn-outline-danger btn-sm mx-1" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $note['id'] }}">
+                        <i class="fa-regular fa-trash-can"></i>
+                    </button>
                 </div>
             </div>
+
             <hr>
             <p class="text-secondary">{{ $note['text'] }}</p>
         </div>
+        <br>
     </div>
 </div>
 
-
+<!-- Modal de confirmação de exclusão -->
+<div class="modal fade" id="deleteModal{{ $note['id'] }}" tabindex="-1" aria-labelledby="deleteModalLabel{{ $note['id'] }}" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-danger text-white">
+                <h5 class="modal-title" id="deleteModalLabel{{ $note['id'] }}">Confirmar exclusão</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Fechar"></button>
+            </div>
+            <div class="modal-body">
+                Tem certeza que deseja excluir a nota <strong>"{{ $note['title'] }}"</strong>?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <a href="{{ route('delete', ['id' => Crypt::encrypt($note['id'])]) }}" class="btn btn-danger">Sim, excluir</a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/top_bar.blade.php
+++ resources/views/top_bar.blade.php
@@ -9,12 +9,37 @@
     </div>
     <div class="col">
         <div class="d-flex justify-content-end align-items-center">
-            <span class="me-3"><i class="fa-solid fa-user-circle fa-lg text-secondary me-3"></i>{{ session('user.username') }}</span>
-            <a href="{{ route('logout') }}" class="btn btn-outline-secondary px-3">
+            <span class="me-3">
+                <i class="fa-solid fa-user-circle fa-lg text-secondary me-3"></i>{{ session('user.username') }}
+            </span>
+
+            <!-- Botão que abre o modal -->
+            <button type="button" class="btn btn-outline-secondary px-3" data-bs-toggle="modal" data-bs-target="#logoutModal">
                 Logout<i class="fa-solid fa-arrow-right-from-bracket ms-2"></i>
-            </a>
+            </button>
         </div>
     </div>
 </div>
 
 <hr>
+
+<!-- Modal de confirmação para logout -->
+<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-warning text-dark">
+                <h5 class="modal-title" id="logoutModalLabel">Confirmar Logout</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+            </div>
+            <div class="modal-body">
+                Tem certeza que deseja sair da sua conta?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <a href="{{ route('logout') }}" class="btn btn-warning text-dark">Sim, sair</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
